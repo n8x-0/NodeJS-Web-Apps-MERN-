f738a5cc118acf0f42384964e6b55477e860e45d
shayan <syedshayan1324@gmail.com>
2025-02-11 19:48:29 +0500

    res.cookie setting updated

--- n8xgram/backend/helper/sessionupdate.js
+++ n8xgram/backend/helper/sessionupdate.js
@@ -2,9 +2,15 @@ const jwt = require("jsonwebtoken")
 
 const sessionUpdate = (updatedUserData, res) => {
     const { _id, username, email, image } = updatedUserData
+    const isProduction = process.env.NODE_ENV === 'production';
     try {
         const token = jwt.sign({ _id, username, email, image }, process.env.JWT_SECRET)
-        res.cookie("session_token", token)
+        res.cookie("session_token", token, {
+            httpOnly: true,
+            secure: isProduction,
+            sameSite: isProduction ? 'strict' : 'lax',
+            path: '/',
+        })
     } catch (error) {
         console.log(error);
         return res.status(500).json({ error: "User session failed." })
