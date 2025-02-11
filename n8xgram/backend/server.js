f1e640d67aa1afe7473667a6aa8a40fb841ca528
shayan <syedshayan1324@gmail.com>
2025-02-11 20:07:41 +0500

    frontend deployed and cors updated

--- n8xgram/backend/server.js
+++ n8xgram/backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE"],
-    origin: ["http://localhost:3000"],
+    origin: "https://n8xgram.vercel.app",
     credentials: true,
 }))
 
