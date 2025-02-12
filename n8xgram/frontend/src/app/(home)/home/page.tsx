9aed206aeb2b87cfa31abf0337bf9facb59c9e0c
shayan <syedshayan1324@gmail.com>
2025-02-12 06:42:23 +0500

    trying some fixes

--- n8xgram/frontend/src/app/(home)/home/page.tsx
+++ n8xgram/frontend/src/app/(home)/home/page.tsx
@@ -48,37 +48,41 @@ const HomePage = () => {
     <div className="max-w-3xl mx-auto p-3 flex-1 max-h-[90vh] overflow-auto hideScrollbar">
       <div className="w-full flex justify-center items-center flex-col">
         {error && <p className="text-red-500 py-6">{error}</p>}
-        {data && data.map((video) => (
-          <div key={video.videoId}>
-            <div className="justify-between w-full p-3 border-zinc-700 border rounded-t-2xl flex items-center gap-2 text-sm">
-              <Link href={`/home/users/${video.author._id}`} className="flex items-center gap-2 font-medium">
-                <div className="w-8 h-8 rounded-full overflow-hidden ring-1 ring-yellow-200">
-                  <Image src={video.author.image} alt="user image" width={400} height={400} className="w-full h-full object-cover" />
+        {data && data.map((video) => {
+          return (
+            <div key={video.videoId}>
+              <div className="justify-between w-full p-3 border-zinc-700 border rounded-t-2xl flex items-center gap-2 text-sm">
+                <div className="flex items-center gap-2 font-medium">
+                  {/* <Link href={`/home/users/${video.author._id}`} className="flex items-center gap-2 font-medium"> */}
+                  <div className="w-8 h-8 rounded-full overflow-hidden ring-1 ring-yellow-200">
+                    <Image src={video.author.image} alt="user image" width={400} height={400} className="w-full h-full object-cover" />
+                  </div>
+                  <p>{video.author.username}</p>
+                  {/* </Link> */}
                 </div>
-                <p>{video.author.username}</p>
-              </Link>
-              <FollowBtn user={video.author} />
-            </div>
-            <div className="w-[520px] h-fit pb-2">
-              <div className="w-full h-[520px] bg-zinc-600">
-                <iframe
-                  src={video.assets.player}
-                  width="100%"
-                  height="100%"
-                  frameBorder="0"
-                  allowFullScreen
-                />
-              </div>              <div className="py-1">
-                <div className="flex items-center gap-3 py-2">
-                  <Heart />
-                  <PlaneIcon />
+                <FollowBtn user={video.author} />
+              </div>
+              <div className="w-[520px] h-fit pb-2">
+                <div className="w-full h-[520px] bg-zinc-600">
+                  <iframe
+                    src={video.assets.player}
+                    width="100%"
+                    height="100%"
+                    frameBorder="0"
+                    allowFullScreen
+                  />
+                </div>              <div className="py-1">
+                  <div className="flex items-center gap-3 py-2">
+                    <Heart />
+                    <PlaneIcon />
+                  </div>
+                  <div className="font-medium">{video.title}</div>
+                  <p className="text-sm">{video.description}</p>
                 </div>
-                <div className="font-medium">{video.title}</div>
-                <p className="text-sm">{video.description}</p>
               </div>
             </div>
-          </div>
-        ))}
+          )
+        })}
       </div>
     </div>
   );
